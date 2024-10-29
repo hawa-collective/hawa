3f6a6a1777347bf21427adfd618d332d866ab4ef
karllucas <karlkamakia@gmail.com>
2024-10-30 01:17:51 +0300

    Updated list of menstrual products

--- data/cups-and-disks.js
+++ data/cups-and-disks.js
@@ -1,6 +1,24 @@
 module.exports = [
-    { brand: "Diva Cup", type: "Menstrual Cup" },
-    { brand: "Moon Cup", type: "Menstrual Cup" },
-    { brand: "Ruby Cup", type: "Menstrual Cup" },
-    { brand: "Saalt Menstrual Cup", type: "Menstrual Cup" },
+  { brand: "DivaCup", type: "Menstrual Cup" },
+  { brand: "MoonCup", type: "Menstrual Cup" },
+  { brand: "Ruby Cup", type: "Menstrual Cup" },
+  { brand: "Saalt Menstrual Cup", type: "Menstrual Cup" },
+  { brand: "Intimina Lily Cup", type: "Menstrual Cup" },
+  { brand: "Lena Cup", type: "Menstrual Cup" },
+  { brand: "Flex Menstrual Disc", type: "Menstrual Disk" },
+  { brand: "Nixit Disc", type: "Menstrual Disk" },
+  { brand: "Cora Menstrual Cup", type: "Menstrual Cup" },
+  { brand: "MeLuna Cup", type: "Menstrual Cup" },
+  { brand: "Super Jennie Cup", type: "Menstrual Cup" },
+  { brand: "Lunette Menstrual Cup", type: "Menstrual Cup" },
+  { brand: "Hello Cup", type: "Menstrual Cup" },
+  { brand: "Ziggy Menstrual Disc", type: "Menstrual Disk" },
+  { brand: "Tampax Cup", type: "Menstrual Cup" },
+  { brand: "Organicup", type: "Menstrual Cup" },
+  { brand: "SckoonCup", type: "Menstrual Cup" },
+  { brand: "FemmyCycle", type: "Menstrual Cup" },
+  { brand: "Kind Cup", type: "Menstrual Cup" },
+  { brand: "Talisi Menstrual Cup", type: "Menstrual Cup" },
+  { brand: "Merula Cup", type: "Menstrual Cup" }
+  
   ];
